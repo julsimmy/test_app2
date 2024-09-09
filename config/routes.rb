4295b131a0f4da87c94755c93d591ecaa1a907f7
julsimmy <jesimko@gmail.com>
2024-09-09 12:20:14 -0500

    seeding the database

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  resources :books
+  
+  root 'books#index
+  '
+  get 'books/index'
+  get 'books/show'
+  get 'books/new'
   get 'main/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root 'main#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
